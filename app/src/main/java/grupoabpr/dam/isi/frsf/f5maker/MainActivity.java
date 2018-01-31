09b6816e8d663e61dbce4c2da6e0512fcb7bb2e2
emma284 <emma_a284@hotmail.com>
2018-01-30 23:51:00 -0300

    Botones y segunda actividad

--- app/src/main/java/grupoabpr/dam/isi/frsf/f5maker/MainActivity.java
+++ app/src/main/java/grupoabpr/dam/isi/frsf/f5maker/MainActivity.java
@@ -1,14 +1,30 @@
 package grupoabpr.dam.isi.frsf.f5maker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button botonGrupos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getSupportActionBar().hide();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botonGrupos = (Button) findViewById(R.id.botonGrupos);
+        //findViewById(R.id.botonGrupos).setOnClickListener((View.OnClickListener) this);
+        botonGrupos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent grupo = new Intent (getApplicationContext(),ActivityListaGrupos.class);
+                startActivity(grupo);
+            }
+        });
+
     }
 }

007ccfcef24742c8fbef7da66670f6f1ab05b18f
Emma <emma_a284@hotmail.com>
2018-03-07 08:01:31 -0300

    Modificaciones en grupo

--- app/src/main/java/grupoabpr/dam/isi/frsf/f5maker/MisHorarios.java
+++ app/src/main/java/grupoabpr/dam/isi/frsf/f5maker/MisHorarios.java
@@ -2,12 +2,18 @@ package grupoabpr.dam.isi.frsf.f5maker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridLayout;
+import android.widget.GridView;
 
 public class MisHorarios extends AppCompatActivity {
-
+    private GridView tablaHorarios;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mis_horarios);
+
+        tablaHorarios = (GridView)findViewById(R.id.gridViewHorarioUsuario);
+
     }
+
 }
